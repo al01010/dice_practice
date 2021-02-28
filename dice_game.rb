c66395d3fd034271b4365549c83b66170de5015d
Alice Lui <aytlui@gmail.com>
2021-02-28 15:00:29 +0000

    first to two

--- dice_game.rb
+++ dice_game.rb
@@ -1,3 +1,4 @@
+puts "First Player to win 2 rounds wins"
 puts "Type 'roll' to roll the die"
 
 # method for rolling the dice
@@ -28,13 +29,27 @@ def play(player)
   end
 end
 
-p1_total = play("Player 1")
-p2_total = play("Player 2")
+p1_score = 0
+p2_score = 0
 
-if p1_total > p2_total
-  puts "Player 1 is the winner"
-elsif p1_total < p2_total
-  puts "Player 2 is the winner"
-else 
-  puts "Draw!"
+until p1_score == 2 || p2_score == 2 do
+  p1_total = play("Player 1")
+  p2_total = play("Player 2")
+
+  if p1_total > p2_total
+    puts "Player 1 wins that round"
+    p1_score += 1
+  elsif p1_total < p2_total
+    puts "Player 2 wins that round"
+    p2_score += 1
+  else 
+    puts "Draw!"
+  end
+  puts "Score: P1 #{p1_score} P2 #{p2_score} "
+end 
+
+if p1_score == 2 
+  puts "Player 1 Wins"
+else
+  puts "Player 2 Wins"
 end
